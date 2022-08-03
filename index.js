8b1be293e56bd245a3cd5f15733b5c9f69031c02
Omarfaruk05 <mdomarfrauk149518.com>
2022-08-04 00:01:52 +0600

    added razorpay

--- index.js
+++ index.js
@@ -1,45 +1,61 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv').config();
-const PaymentRoutes = require("./routes/payment")
-const port = process.env.PORT || 5000;
-
-
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const Razorpay = require("razorpay");
+const crypto = require("crypto");
 const app = express();
 
+dotenv.config();
 
-app.use(express());
+app.use(express.json());
 app.use(cors());
 
-app.post('/orders', async(req, res) => {
-    try {
-        const instance = new Razorpay({
-            key_id: process.env.KEY_ID,
-            key_secret: process.env.KEY_SECRET,
-        });
-        console.log(process.env.KEY_ID, process.env.KEY_SECRET)
-        const options = {
-            amount: req.body.amount * 100,
-            currency: 'INR',
-            receipt:crypto.randomBytes(10).toString("hex"),
-        };
+app.post("/orders", async (req, res) => {
+	try {
+		const instance = new Razorpay({
+			key_id: process.env.KEY_ID,
+			key_secret: process.env.KEY_SECRET,
+		});
 
-        instance.orders.create(options, (error, order) => {
-          if(error){
-            console.log(error);
-            return res.send(500).json({message:'Something is wrong!'});  
-          }
-          res.status(200).json({data:order});
-        });
-    } 
-    catch (error) {
-        console.log(error);
-        return res.send(500).json({message:'Internal Server Error'}); 
+		const options = {
+			amount: req.body.amount * 100,
+			currency: "INR",
+			receipt: crypto.randomBytes(10).toString("hex"),
+		};
 
-    }
+		instance.orders.create(options, (error, order) => {
+			if (error) {
+				console.log(error);
+				return res.status(500).json({ message: "Something Went Wrong!" });
+			}
+			res.status(200).json({ data: order });
+		});
+	} catch (error) {
+		res.status(500).json({ message: "Internal Server Error!" });
+		console.log(error);
+	}
 });
 
-app.get('/', (req, res) =>{
-    res.send('e shop is running')
-})
-app.listen(port, () => console.log(`Listening on port ${port}`))
+app.post("/verify", async (req, res) => {
+	try {
+		const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+			req.body;
+		const sign = razorpay_order_id + "|" + razorpay_payment_id;
+		const expectedSign = crypto
+			.createHmac("sha256", process.env.KEY_SECRET)
+			.update(sign.toString())
+			.digest("hex");
+
+		if (razorpay_signature === expectedSign) {
+			return res.status(200).json({ message: "Payment verified successfully" });
+		} else {
+			return res.status(400).json({ message: "Invalid signature sent!" });
+		}
+	} catch (error) {
+		res.status(500).json({ message: "Internal Server Error!" });
+		console.log(error);
+	}
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
