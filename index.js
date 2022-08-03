a5ddd8f63f7a94d3b1697cd77b0501318543f126
Omarfaruk05 <mdomarfrauk149518.com>
2022-08-03 22:31:53 +0600

    added key

--- index.js
+++ index.js
@@ -11,6 +11,35 @@ const app = express();
 app.use(express());
 app.use(cors());
 
-app.use('api/payment/', PaymentRoutes);
+app.post('/orders', async(req, res) => {
+    try {
+        const instance = new Razorpay({
+            key_id: process.env.KEY_ID,
+            key_secret: process.env.KEY_SECRET,
+        });
+        console.log(process.env.KEY_ID, process.env.KEY_SECRET)
+        const options = {
+            amount: req.body.amount * 100,
+            currency: 'INR',
+            receipt:crypto.randomBytes(10).toString("hex"),
+        };
 
+        instance.orders.create(options, (error, order) => {
+          if(error){
+            console.log(error);
+            return res.send(500).json({message:'Something is wrong!'});  
+          }
+          res.status(200).json({data:order});
+        });
+    } 
+    catch (error) {
+        console.log(error);
+        return res.send(500).json({message:'Internal Server Error'}); 
+
+    }
+});
+
+app.get('/', (req, res) =>{
+    res.send('e shop is running')
+})
 app.listen(port, () => console.log(`Listening on port ${port}`))
