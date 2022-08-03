354ad339ffd8f7c97396d63d5ffda71c6225b6d2
Omarfaruk05 <mdomarfrauk149518.com>
2022-08-03 19:14:36 +0600

    added razorpay

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
+const PaymentRoutes = require("./routes/payment")
 const port = process.env.PORT || 5000;
 
 
 const app = express();
+
+
+app.use(express());
+app.use(cors());
+
+app.use('api/payment/', PaymentRoutes);
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
